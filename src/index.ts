1ea57e13f695ba89745d35ee203e19ad4f3f0826
郑温剑 <904656778@qq.com>
2020-07-25 17:36:48 +0800

    selection and range 实现失焦后设置文本样式

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,14 @@ import "./index.less";
 class Editor {
   private options: Editor.options;
   private dom: HTMLDivElement | null;
+  private selection: Selection | null | undefined;
+  private range: Range | undefined;
+
   constructor(options: Editor.options) {
     this.options = options;
     const { el } = options;
     this.dom = document.querySelector(el);
-
+    this.selection = window.getSelection();
     this.init();
   }
 
@@ -17,35 +20,61 @@ class Editor {
     this.dom?.setAttribute(key, value);
   }
 
-  init() {
-    this.attr("contenteditable", "true");
-    this.attr("class", "KEditor");
+  exec(cmd: string, showUI: boolean = false, value: string = "") {
+    document.execCommand(cmd, showUI, value);
+  }
 
+  setRange() {
+    const { selection } = this;
+    if (selection && !selection?.isCollapsed) {
+      const range = selection?.getRangeAt(0);
+      console.log("selection", selection?.toString(), selection, range);
+      this.range = range;
+    }
+  }
+
+  recoverRange() {
+    const { selection, range } = this;
+
+    if (selection && range) {
+      if (selection.rangeCount > 0) selection.removeAllRanges();
+      selection?.addRange(range.cloneRange());
+    }
+  }
+
+  init() {
     const that = this;
 
-    this.dom?.before(
+    that.attr("contenteditable", "true");
+    that.attr("class", "KEditor");
+
+    that.dom?.before(
       createElement("button", "bold", that.setBold.bind(that)),
       createElement("button", "color", that.setColor.bind(that)),
       createElement("button", "bgColor", that.setBgColor.bind(that)),
       createElement("button", "alertHtml", that.getHtml.bind(that)),
       createElement("button", "alertText", that.getText.bind(that))
     );
-  }
 
-  exec(cmd: string, showUI: boolean = false, value: string = "") {
-    document.execCommand(cmd, showUI, value);
+    //
+    that.dom?.addEventListener("blur", () => {
+      that.setRange();
+    });
   }
 
   setBold() {
+    this.recoverRange();
     this.exec("bold");
   }
 
   setColor() {
+    this.recoverRange();
     const color = prompt("请输入颜色") ?? "#000";
     this.exec("foreColor", false, color);
   }
 
   setBgColor() {
+    this.recoverRange();
     const color = prompt("请输入颜色") ?? "#000";
     this.exec("backColor", true, color);
     console.log("setBgColor", color);
